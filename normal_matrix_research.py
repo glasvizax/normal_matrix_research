66608d731f6b065434e7c50736a921c4c8eac448
DESKTOP-DF633JQ\glasvisax <glasvizax@gmail.com>
2026-06-16 13:14:47 +0300

    fun

--- normal_matrix_research.py
+++ normal_matrix_research.py
@@ -1,11 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig, ax = plt.subplots()
-
-def draw_line(vec1: np.ndarray, vec2: np.ndarray, n: int):
-    global fig, ax
-    
+def draw_line(vec1: np.ndarray, vec2: np.ndarray, n: int, ax : plt.Axes): 
     if(vec1[0] > vec2[0]):
         vec1, vec2 = vec2, vec1
         
@@ -28,22 +24,37 @@ norm = norm3d[0:2]
 
 vec_norm_start = vec2 + (tang / 2)
 
-draw_line(vec1, vec2, interp_sz)
-draw_line(vec_norm_start, vec_norm_start + norm, interp_sz)
+scale = np.array([2.0, 2.0, 1.0])
 
-scale = np.array([2.0, 3.0, 1.0])
-scale_inv = 1 / scale
+lserr = np.linspace(0, 10, 100)
+err_array = np.empty(np.size(lserr))
 
-scale_mat = np.diag(scale)
-scale_mat_trans_inv =  np.diag(scale_inv)
+err_array
 
-tang3d_scaled = scale_mat @ tang3d.transpose()
-norm3d_scaled = scale_mat_trans_inv @ norm3d.transpose()
+for idx, i in enumerate(lserr):
+    curr_scale = scale.copy()
+    curr_scale[1] += i
+    scale_inv = 1 / curr_scale
 
-tang_norm_dot = np.dot(tang3d, norm3d) # 0
-tang_norm_scaled_dot = np.dot(tang3d_scaled, norm3d_scaled) # 0
+    scale_mat = np.diag(curr_scale)
+    scale_mat_trans_inv =  np.diag(scale_inv)
 
-print(tang_norm_scaled_dot)
+    tang3d_scaled = scale_mat @ tang3d.transpose()
+    norm3d_scaled = scale_mat @ norm3d.transpose()
+    norm3d_scaled_fix = scale_mat_trans_inv @ norm3d.transpose()
 
+    tang_norm_dot = np.dot(tang3d, norm3d) # 0
+    tang_norm_scaled_dot_fix = np.dot(tang3d_scaled, norm3d_scaled_fix) # 0
+    tang_norm_scaled_dot = np.dot(tang3d_scaled, norm3d_scaled) # ?
+
+    norm_normfix_dot = np.dot(norm3d_scaled_fix, norm3d_scaled)
+    
+    err_array[idx] = norm_normfix_dot
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+draw_line(vec1, vec2, interp_sz, ax1)
+draw_line(vec_norm_start, vec_norm_start + norm, interp_sz, ax1)
+
+ax2.plot(lserr, err_array)
 plt.show()
 
